deaea0d5818d18cb1fa64182f3ff93099ee88ef5
Andrea Lizeth Quishpe Zapata <38335472+AndreaLizeth@users.noreply.github.com>
2018-10-10 20:16:18 -0500

    Add files via upload

---
+++ Quishpe_Andrea_Deber_1/main.py
@@ -0,0 +1,17 @@
+print("Ingrese nombre(1):")
+nombre1= input()
+print("Ingrese la edad(1):")
+edad1= input()
+edad1= int (edad1)
+
+print("Ingrese nombre(2):")
+nombre2= input()
+print("Ingrese la edad(2):")
+edad2= input()
+edad2= int (edad2)
+
+if edad1 > edad2 :
+  print(nombre1+ " es mayor a "+ nombre2)
+else :
+    print(nombre2+" es mayor a "+ nombre1)
+
